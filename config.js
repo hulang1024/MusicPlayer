88a64865779e8711ca4836254f5da22a48717f4b
hulang1024 <1013644379@qq.com>
2019-11-28 13:38:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var CONFIG = {
-songsUrl: 'https://raw.githubusercontent.com/hulang1024/MusicPlayer/res/songs.json'
+songsUrl: 'https://raw.githubusercontent.com/hulang1024/MusicPlayer/res/songs.json?v=20191128'
 //songsUrl: 'http://og11a17b0.bkt.clouddn.com/songs.json'
 
 };
